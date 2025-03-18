02fc9a6e19f1ed0f947dcdf66ee3867a310fafae
SimoRada <162886844+SimoRada@users.noreply.github.com>
2025-03-18 17:39:29 +0100

    aggiunta dei files
    ho aggiunto per la prima volta i file

---
+++ index.js
@@ -0,0 +1,26 @@
+
+let countEl = document.getElementById("count-el")
+let missEl = document.getElementById("miss-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+let total = 0
+let stats
+//let ciao = count + "/" + total
+function increment(){
+    total += 1
+    count += 1
+    countEl.textContent = count + "/" + total
+}
+function miss(){
+    total += 1
+    countEl.textContent = count + "/" + total
+}
+function save(){
+    stats = (count / total) * 100
+    let countStr = count + "/" + total  + "(" + stats + "%) <br>" 
+    saveEl.innerHTML += countStr 
+    //innerText vs textContent, look Modzilla
+    count = 0
+    total = 0
+    countEl.textContent = count + "/" + total 
+}
